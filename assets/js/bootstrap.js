55e8a11befd85184dde35671e465bb1dda9e43a0
revilorasec <cesar.oliver@gmail.com>
2026-09-14 15:45:00 -0300

    fix: carregar instalacao PWA v24

--- assets/js/bootstrap.js
+++ assets/js/bootstrap.js
@@ -5,7 +5,7 @@ async function start() {
   if (next) {
     await import('./next/app-next.js?v=20');
     await import('./next/medical-appointments-v23.js?v=23');
-    await import('./next/pwa-install-v23.js?v=23');
+    await import('./next/pwa-install-v24.js?v=24');
   } else {
     await new Promise((resolve, reject) => {
       const script = document.createElement('script');
